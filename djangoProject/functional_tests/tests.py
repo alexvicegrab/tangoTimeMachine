b15eef2c704006d41d70584598b1394ecad22850
Sasha <a.vicente.grab@gmail.com>
2015-07-29 17:27:05 +0200

    Updated functional test

--- djangoProject/functional_tests/tests.py
+++ djangoProject/functional_tests/tests.py
@@ -22,35 +22,57 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         
         # Page header for TTM
-        self.assertIn('Tango Ads', self.browser.title)
+        self.assertIn("Tango Ads", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Tango Ads', header_text)
+        self.assertIn("Tango Ads", header_text)
         
         # Should be able to input a Tango Event into the TTM. 
         inputbox = self.browser.find_element_by_id('id_new_event')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a tango event'
+            "Enter a tango event"
         )
         
         # Input an example: "Di Sarli, Carlos"
-        inputbox.send_keys('Di Sarli, Carlos')
+        inputbox.send_keys("Di Sarli, Carlos")
         inputbox.send_keys(Keys.ENTER)
+        
+        page1_list_url = self.browser.current_url
+        self.assertRegex(page1_list_url, "/pages/.+")
         
         # Update page to list the single Tango Event
         # Now the page should read "Di Sarli, Carlos"
-        self.check_for_row_in_list_table('1. Di Sarli, Carlos')
+        self.check_for_row_in_list_table("1. Di Sarli, Carlos")
         
         # Should still be able to to add an extra item
         # * "Troilo, Anibal"
         inputbox = self.browser.find_element_by_id('id_new_event')
-        inputbox.send_keys('Troilo, Anibal')
+        inputbox.send_keys("Troilo, Anibal")
         inputbox.send_keys(Keys.ENTER)
         
         # Update page to list both Events
-        self.check_for_row_in_list_table('1. Di Sarli, Carlos')
-        self.check_for_row_in_list_table('2. Troilo, Anibal')
+        self.check_for_row_in_list_table("1. Di Sarli, Carlos")
+        self.check_for_row_in_list_table("2. Troilo, Anibal")
         
-        # Refreshing the page should show that the list of events is still there
+        # A different page should have no events [start by implementing no DB model]
+        ## New browser session to ensure no info of page1 comes through cookies, etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
         
-        self.fail('Finish the test!')
+        # We start adding new events to this page
+        inputbox = self.browser.find_element_by_id('id_new_event')
+        inputbox.send_keys("D'Arienzo, Juan")
+        inputbox.send_keys(Keys.ENTER)
+        
+        # This page also has a URL
+        page2_list_url = self.browser.current_url
+        self.assertRegex(page2_list_url, "/pages/.+")
+        
+        # This should be a different URL
+        self.assertNotEqual(page1_list_url, page2_list_url)
+        
+        # And it does not contain the items in the 1st page, but those in 2nd page
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Di Sarli, Carlos", page_text)
+        self.assertIn("D'Arienzo, Juan", page_text)
+        
